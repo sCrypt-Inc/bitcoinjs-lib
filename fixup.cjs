429e7df7d68af60920a091521cf04d877da0b7be
hh <hf.zheng@scrypt.io>
2025-01-21 09:37:37 +0800

    update fixup.cjs

--- fixup.cjs
+++ fixup.cjs
@@ -4,8 +4,8 @@ const path = require('path');
 const updateRequires = (filePath) => {
   let content = fs.readFileSync(filePath, 'utf8');
   //replace local imports eg. require("./ecpair.js") to require("ecpair.cjs")
-  content = content.replace(/require\('\.\/([^']*)\.js'\)/g, "require('./$1.cjs')");
-  content = content.replace(/require\('\.\.\/([^']*)\.js'\)/g, "require('../$1.cjs')");
+  content = content.replace(/require\("\.\/([^"]*)\.js"\)/g, "require('./$1.cjs')");
+  content = content.replace(/require\("\.\.\/([^"]*)\.js"\)/g, "require('../$1.cjs')");
 
   fs.writeFileSync(filePath, content, 'utf8');
 };
